bebf7a9146624cd1ce783779c13c92c499360c45
Joshua Cho <joshdavecho@protonmail.com>
2021-01-18 18:56:14 -0500

    commit tree 9f03ac253815ac4d6999e08137ed2aad004132e5

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+    if block_given?
+        i = 0
+        collection = []
+        while i < array.size
+            collection << yield(array[i])
+            i += 1
+        end
+        
+        return collection
+    else return "No block was given."
+    end
+end
 
-
+    
